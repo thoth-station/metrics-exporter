1529698388d9e7a45521e0236cbb01ff98ce51f5
Christoph Görn <goern@redhat.com>
2018-10-18 01:32:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ if __name__ == '__main__':
     scheduler.init_app(application)
     scheduler.start()
 
-    application.run(port=8080, debug=_DEBUG)
+    application.run(host='0.0.0.0', port=8080, debug=_DEBUG)
