b4f23e4471167f5865b204352eb9dc207126f46e
Christoph Görn <goern@redhat.com>
2018-10-28 12:45:50 +0100

    fixing the fixable
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/metrics_exporter/__init__.py
+++ thoth/metrics_exporter/__init__.py
@@ -23,7 +23,7 @@ from prometheus_client import Gauge, Summary
 from thoth.common import __version__ as __common__version__
 from thoth.storages import __version__ as __storages__version__
 
-__version__ = f"0.4.0+storage.{__storages__version__}+common.{__common__version__}"
+__version__ = f"0.4.0+storage.{__storages__version__}.common.{__common__version__}"
 __author__ = "Christoph Görn <goern@redhat.com>"
 
 thoth_metrics_exporter_info = Gauge('thoth_metrics_exporter_info',
