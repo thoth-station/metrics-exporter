1ff028d1633bab2ed86c65765f67e7c42861f065
Francesco Murdaca <fmurdaca@redhat.com>
2020-01-02 13:53:00 +0100

    Created inspection latency SLI
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/metrics_exporter/jobs/inspection.py
+++ thoth/metrics_exporter/jobs/inspection.py
@@ -18,10 +18,14 @@
 """Inspection metrics."""
 
 import logging
+import os
+from datetime import datetime
+
+import thoth.metrics_exporter.metrics as metrics
+from prometheus_api_client import PrometheusConnect
 
 from thoth.storages import InspectionResultsStore
 from thoth.storages.graph.enums import SoftwareStackTypeEnum
-import thoth.metrics_exporter.metrics as metrics
 
 from .base import register_metric_job
 from .base import MetricsBase
@@ -31,6 +35,15 @@ _LOGGER = logging.getLogger(__name__)
 
 class InspectionMetrics(MetricsBase):
     """Class to evaluate Metrics for Amun Inspections."""
+
+    _URL = "https://prometheus-dh-prod-monitoring.cloud.datahub.psi.redhat.com"
+    _PROMETHEUS_SERVICE_ACCOUNT_TOKEN = os.environ["PROMETHEUS_SERVICE_ACCOUNT_TOKEN"]
+    _HEADERS = {"Authorization": f"bearer {_PROMETHEUS_SERVICE_ACCOUNT_TOKEN}"}
+    _NAMESPACE = os.environ["THOTH_FRONTEND_NAMESPACE"]
+
+    _PROM = PrometheusConnect(url=_URL, disable_ssl=True, headers=_HEADERS)
+
+    _INSPECTION_CHECK_TIME = datetime.now()
 
     @staticmethod
     @register_metric_job
@@ -64,3 +77,53 @@ class InspectionMetrics(MetricsBase):
         )
         metrics.graphdb_inspection_software_stacks_records.set(thoth_graphdb_total_inspection_software_stacks)
         _LOGGER.debug("graphdb_inspection_software_stacks_records=%r", thoth_graphdb_total_inspection_software_stacks)
+
+    @classmethod
+    @register_metric_job
+    def get_inspection_evaluation_time(cls) -> None:
+        """Get the time spent for each inspection worflow."""
+        workflow_completion_time_metric_name = "argo_workflow_completion_time"
+        workflow_completion_time = cls._PROM.get_current_metric_value(
+            metric_name=workflow_completion_time_metric_name,
+            label_config={
+                'instance': f"workflow-controller-metrics-{cls._NAMESPACE}.cloud.paas.psi.redhat.com:80",
+                "namespace": cls._NAMESPACE}
+                )
+
+        if workflow_completion_time:
+            inspection_workflows = {}
+            for metric in workflow_completion_time:
+                if "inspection" in metric['metric']['name']:
+                    completion_time = datetime.fromtimestamp(
+                            int(metric['value'][1])
+                            )
+                    new_time = datetime.now()
+
+                    if cls._INSPECTION_CHECK_TIME < completion_time < new_time: 
+                        inspection_workflows[metric['metric']['name']] = completion_time
+
+            cls._INSPECTION_CHECK_TIME = new_time
+
+            if inspection_workflows:
+                for workflow_name, completion_time in inspection_workflows.items():
+                    workflow_start_time_metric_name = "argo_workflow_start_time"
+                    workflow_start_time = cls._PROM.get_current_metric_value(
+                        metric_name=workflow_start_time_metric_name,
+                        label_config={
+                            'instance': f"workflow-controller-metrics-{cls._NAMESPACE}.cloud.paas.psi.redhat.com:80",
+                            "namespace": cls._NAMESPACE,
+                            "name": workflow_name}
+                            )
+
+                    start_time = datetime.fromtimestamp(
+                        int(workflow_start_time[0]['value'][1])
+                        )
+                    metrics.workflow_inspection_latency.observe(
+                        (completion_time - start_time).total_seconds()
+                        )
+
+            else:
+                _LOGGER.debug("No inspection workflow identified")
+
+        else:
+            _LOGGER.debug("No metrics identified for %r", workflow_completion_time_metric_name)
