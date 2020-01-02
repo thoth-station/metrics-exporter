51a0a2c572a1c189f4b88e2cb0f0a5b33c18ee5f
Francesco Murdaca <fmurdaca@redhat.com>
2020-01-02 13:52:42 +0100

    Created adviser latency SLI
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/metrics_exporter/jobs/adviser.py
+++ thoth/metrics_exporter/jobs/adviser.py
@@ -18,10 +18,13 @@
 """Adviser metrics."""
 
 import logging
+import os
+from datetime import datetime
 
 from thoth.storages import GraphDatabase
 from thoth.storages.graph.enums import SoftwareStackTypeEnum
 import thoth.metrics_exporter.metrics as metrics
+from prometheus_api_client import PrometheusConnect
 
 from .base import register_metric_job
 from .base import MetricsBase
@@ -32,6 +35,15 @@ _LOGGER = logging.getLogger(__name__)
 class AdviserMetrics(MetricsBase):
     """Class to evaluate Metrics for Adviser."""
 
+    _URL = "https://prometheus-dh-prod-monitoring.cloud.datahub.psi.redhat.com"
+    _PROMETHEUS_SERVICE_ACCOUNT_TOKEN = os.environ["PROMETHEUS_SERVICE_ACCOUNT_TOKEN"]
+    _HEADERS = {"Authorization": f"bearer {_PROMETHEUS_SERVICE_ACCOUNT_TOKEN}"}
+    _NAMESPACE = os.environ["THOTH_FRONTEND_NAMESPACE"]
+
+    _PROM = PrometheusConnect(url=_URL, disable_ssl=True, headers=_HEADERS)
+
+    _ADVISER_CHECK_TIME = 0
+
     @classmethod
     @register_metric_job
     def get_advised_python_software_stack_count(cls) -> None:
@@ -41,3 +53,53 @@ class AdviserMetrics(MetricsBase):
         )
         metrics.graphdb_advised_software_stacks_records.set(thoth_graphdb_total_advised_software_stacks)
         _LOGGER.debug("graphdb_advised_software_stacks_records=%r", thoth_graphdb_total_advised_software_stacks)
+
+    @classmethod
+    @register_metric_job
+    def get_adviser_evaluation_time(cls) -> None:
+        """Get the time spent for each adviser worflow."""
+        workflow_completion_time_metric_name = "argo_workflow_completion_time"
+        workflow_completion_time = cls._PROM.get_current_metric_value(
+            metric_name=workflow_completion_time_metric_name,
+            label_config={
+                'instance': f"workflow-controller-metrics-{cls._NAMESPACE}.cloud.paas.psi.redhat.com:80",
+                "namespace": cls._NAMESPACE}
+                )
+
+        if workflow_completion_time:
+            inspection_workflows = {}
+            for metric in workflow_completion_time:
+                if "adviser" in metric['metric']['name']:
+                    completion_time = datetime.fromtimestamp(
+                            int(metric['value'][1])
+                            )
+                    new_time = datetime.now()
+
+                    if cls._ADVISER_CHECK_TIME < completion_time < new_time: 
+                        inspection_workflows[metric['metric']['name']] = completion_time
+
+            cls._ADVISER_CHECK_TIME = new_time
+
+            if inspection_workflows:
+                for workflow_name, completion_time in inspection_workflows.items():
+                    workflow_start_time_metric_name = "argo_workflow_start_time"
+                    workflow_start_time = cls._PROM.get_current_metric_value(
+                        metric_name=workflow_start_time_metric_name,
+                        label_config={
+                            'instance': f"workflow-controller-metrics-{cls._NAMESPACE}.cloud.paas.psi.redhat.com:80",
+                            "namespace": cls._NAMESPACE,
+                            "name": workflow_name}
+                            )
+
+                    start_time = datetime.fromtimestamp(
+                        int(workflow_start_time[0]['value'][1])
+                        )
+                    metrics.workflow_adviser_latency.observe(
+                        (completion_time - start_time).total_seconds()
+                        )
+
+            else:
+                _LOGGER.debug("No adviser workflow identified")
+
+        else:
+            _LOGGER.debug("No metrics identified for %r", workflow_completion_time_metric_name)
